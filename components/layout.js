104d04dafc0d6a34c4af069aa61b5cc129bcc9d2
SupaDupa24 <dequansanders17@gmail.com>
2021-09-01 23:16:45 -0500

    part6
    moved to index

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,15 @@
 import Alert from '../components/alert'
 import Footer from '../components/footer'
 import Meta from '../components/meta'
+import Particles from 'react-particles-js'
+import particlesOptions from '../components/particles'
 
 export default function Layout({ preview, children }) {
   return (
     <>
       <Meta />
       <div className="min-h-screen">
+      <Particles params={{particlesOptions}} />
         <main>{children}</main>
       </div>
       <Footer />
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 import '../styles/index.css'
-import Particles from 'react-particles-js'
-import particlesOptions from '../components/particles'
+
 
 
 function MyApp({ Component, pageProps }) {
   return 
   <React.Fragment>
-    <Particles params={{particlesOptions}} />
     <Component {...pageProps} />
   </React.Fragment>
 }
