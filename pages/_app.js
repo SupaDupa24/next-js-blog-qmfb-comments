9a541da3250357dce89704d73386e0ad5601bfe8
SupaDupa24 <dequansanders17@gmail.com>
2021-09-01 21:58:58 -0500

    part1
    part1

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/index.css'
+import Particles from 'react-particles-js'
+import particlesOptions from '../components/particles'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return 
+  <React.Fragment>
+    <Particles
+        params={particlesOptions} />
+    <Component {...pageProps} />
+  </React.Fragment>
 }
 
 export default MyApp
